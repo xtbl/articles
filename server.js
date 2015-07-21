9581786e6b00828941393c250de8cea8b458dd52
xtbl <correodecristobal@gmail.com>
2015-07-20 21:36:03 -0600

    Articles model added

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Article = require('./app/models/article');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://test:testcrazy@dbh74.mongolab.com:27747/articles');
+
 
 var port = process.env.PORT || 8080;
 
 // ROUTES
 // ===========================================================================
 var router = express.Router();
+
+// middleware for all requests
+router.use(function(req, res, next) {
+	console.log('Something is happening');
+	next();
+});
 
 router.get('/', function(req, res) {
 	res.json({message: 'Welcome!'});
