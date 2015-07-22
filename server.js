3b69960881a1bd74ea7344faa38b52b8684b2d47
xtbl <correodecristobal@gmail.com>
2015-07-21 20:32:32 -0600

    post article, get articles

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Article = require('./app/models/article');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://test:testcrazy@dbh74.mongolab.com:27747/articles');
+mongoose.connect('mongodb://test:testxxxxx@dbh74.mongolab.com:27747/articles');
 
 
 var port = process.env.PORT || 8080;
@@ -27,6 +27,26 @@ router.get('/', function(req, res) {
 });
 
 // more routes
+
+router.route('/articles')
+	.post(function(req, res) {
+		var article = new Article();
+		article.name = req.body.name;
+		article.save(function(err) {
+			if(err) 
+				res.send(err);
+		
+			res.json({message: 'Article created'});
+		});
+	})
+	.get(function(req, res) {
+		Article.find(function(err, articles) {
+			if(err)
+				res.send(err);
+
+			res.json(articles);
+		});
+	});
 
 // REGISTER ROUTES -----------------
 // routes are prefixed with /api
