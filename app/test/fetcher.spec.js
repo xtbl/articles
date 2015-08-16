33138f5714e88d95edc50ca0e5db75febf1cec5f
xtbl <correodecristobal@gmail.com>
2015-08-15 22:45:02 -0600

    formatIntoArticle method added

--- app/test/fetcher.spec.js
+++ app/test/fetcher.spec.js
@@ -1,11 +1,15 @@
 var should = require("should");
+var mockFs = require("mock-fs");
 var Fetcher = require("../utils/fetcher");
+var rawContentMock = require("../test/rawContentMockRedditComics.json");
+
 describe("Fetcher", function () {
    var fetcher = {};
-
+    var formattedList = [];
    before(function () {
-      fetcher = new Fetcher();
-       fetcher.setConfigUrl("http://google.com")
+       fetcher = new Fetcher();
+       fetcher.setConfigUrl("http://google.com");
+       formattedList = fetcher.formatIntoArticle(rawContentMock, "reddit");
    })
 
     it("has fetch url", function () {
@@ -13,5 +17,16 @@ describe("Fetcher", function () {
     });
     it("gets content list");
     it("gets content list from file");
+    it("returns a formatted list of articles", function () {
+        //TODO: add this test and mock-fs file reader test
+        // to populate the db with the formatted list from the file
+        //but first create the formatter, thats the priority
+        //important field in reddit: post_hint: "image"
+        //console.log(rawContentMock);
+        console.log(formattedList);
+      //  formattedList[0].title.should.equal("[Meta] /r/Comics is looking for moderators, apply within");
+    });
+
+
 });
 
--- app/utils/fetcher.js
+++ app/utils/fetcher.js
@@ -27,6 +27,33 @@ var Fetcher = function () {
         })
     }
 
+    function formatIntoArticle(rawContentList, sourceType) {
+       //map required properties and return
+        var redditComic = function (rawItem) {
+            var rawItemData = rawItem.data;
+            var getImage = function(rawItemPreview){
+                return _(rawItemPreview.images).pluck("source").pluck("url").value()[0];
+            };
+            return {
+                title: rawItemData.title,
+                description: rawItemData.title || "",
+                url: rawItemData.url || "",
+                source: rawItemData.source || "",
+                thumbnail: rawItemData.thumbnail || "",
+                image1: getImage(rawItemData.preview),
+                post_hint: rawItemData.post_hint || ""
+            }
+        };
+        var filterImageItems = function (nonFilteredItem) {
+            return (nonFilteredItem.data.hasOwnProperty("post_hint") && nonFilteredItem.data.post_hint === "image");
+        }
+        if(sourceType === 'reddit') {
+            //return _(rawContentList.data.children).filter(filterImageItems);//.map(redditComic);
+            //return _.filter(rawContentList.data.children, filterImageItems);
+            return _(rawContentList.data.children).filter(filterImageItems).map(redditComic).value();
+        }
+    }
+
     function getConfig() {
         return config;
     }
@@ -35,7 +62,8 @@ var Fetcher = function () {
         setConfigUrl: setConfigUrl,
         getConfig: getConfig,
         getContentList: getContentList,
-        getContentListFromFile: getContentListFromFile
+        getContentListFromFile: getContentListFromFile,
+        formatIntoArticle: formatIntoArticle
     };
 };
 
